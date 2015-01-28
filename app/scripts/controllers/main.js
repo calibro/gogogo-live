86cbd3ea188021554595e4a4399be72e16924c29
uf0 <scnepa222@gmail.com>
2015-01-28 14:51:53 +0100

    added api service and minor changes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,5 @@
  */
 angular.module('gogogoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
   });
---
+++ app/scripts/services/api.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name gogogoApp.api
+ * @description
+ * # api
+ * Factory in the gogogoApp.
+ */
+angular.module('gogogoApp')
+  .factory('api', function ($http, $q) {
+
+    var BASE_API_URL = 'http://188.226.184.134';
+
+    return {
+     getRoutes : function(teamID){
+       var deferred = $q.defer();
+       var serviceUrl = '/getdataapp';
+
+       $http({
+          method: 'GET',
+          url : BASE_API_URL + (teamID ? serviceUrl + "/" + teamID : serviceUrl),
+        }).success(function(data){
+         deferred.resolve(data);
+       }).error(function(){
+         deferred.reject("An error occured while fetching data");
+       });
+
+       return deferred.promise;
+     }
+    };
+  });
